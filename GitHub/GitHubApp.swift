bf14f63268502d4c614803dbb02b75435b07cbc0
Stella Hung <stella.hung2599@hotmail.com>
2023-02-15 09:56:16 +1100

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Stella Hung on 15/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
